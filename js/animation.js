9dbd00066854cc0332823ceb248fdfe0092a5992
Yangseongwoo <163240206+Yangseongwoo@users.noreply.github.com>
2024-11-19 00:28:59 +0900

    update

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+/* document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.header');
+    const infoTitle = document.querySelector('.info_title');
+
+    // 4초 후 헤더 페이드인
+    setTimeout(() => {
+        header.classList.add('fade-in');
+        
+        // 헤더 페이드인 2초 후 info_title 페이드인
+        setTimeout(() => {
+            infoTitle.classList.add('fade-in');
+        }, 2000);
+    }, 4000);
+});  */
